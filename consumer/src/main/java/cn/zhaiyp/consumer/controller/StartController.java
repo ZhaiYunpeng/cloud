8895ecc8a900934755ce547309b5b05ba11f6c13
ZhaiYunpeng <31876335+ZhaiYunpeng@users.noreply.github.com>
2019-09-03 15:42:28 +0800

    xiuai wenjian

--- consumer/src/main/java/cn/zhaiyp/consumer/controller/StartController.java
+++ consumer/src/main/java/cn/zhaiyp/consumer/controller/StartController.java
@@ -2,6 +2,7 @@ package cn.zhaiyp.consumer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author fkptxmz
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class StartController {
 
+    @ResponseBody
     @RequestMapping("/login")
     public String getJsp() {
         System.out.println("test");
