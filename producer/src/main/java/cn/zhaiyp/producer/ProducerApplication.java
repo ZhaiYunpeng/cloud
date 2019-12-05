299829573e39c3ced0efc0042742231f3b426601
ZhaiYunpeng <31876335+ZhaiYunpeng@users.noreply.github.com>
2019-12-05 17:01:58 +0800

    修改IP为localhost

--- producer/src/main/java/cn/zhaiyp/producer/ProducerApplication.java
+++ producer/src/main/java/cn/zhaiyp/producer/ProducerApplication.java
@@ -2,10 +2,10 @@ package cn.zhaiyp.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class ProducerApplication {
 
     public static void main(String[] args) {
